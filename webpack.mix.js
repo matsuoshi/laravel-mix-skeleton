c4a6db4e7e53060043cb14a9ed57f0a07360d528
matsuoshi <matsuo@monaural.net>
2018-09-01 00:00:56 +0900

    Update webpack.mix.js
    sass, fix background-image error

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,9 @@ const dist = './public';
 
 mix.js(`${src}/scripts/app.js`, `${dist}/assets/js`)
   .sass(`${src}/styles/styles.scss`, `${dist}/assets/css`)
+  .options({
+      processCssUrls: false
+  })
   .sourceMaps(false)
   .browserSync({
     files: `${dist}/**/*`,
